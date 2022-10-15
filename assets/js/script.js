c15d41ab681ac1fc11e1902edb14b67039b61603
Supapun Wichachuai <Supapun.Wichachuai@Microchip.com>
2022-10-14 23:14:13 -0700

    adding multiple files

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// display today's date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").text(todayDate);
+
+// saveBtn click 
+$(".saveBtn").on("click", function () {
+    //get values from the same row
+    var text = $(this).siblings(".description").val();
+    var id = $(this).parent().attr("id");
+
+    //save text in local storage
+    localStorage.setItem(id, text);
+})
+
+//display saved data
+$('.description').each(function (i,p) {
+    var id = $(this).parent().attr("id");
+    $(this).val((localStorage.getItem(id)));
+})
+//$("#8 .description").val(localStorage.getItem("8"));
+
+function timeTracker() {
+
+    var timeNow = moment().hour();
+
+    // loop time blocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id"));
+
+        // check time for background color
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+timeTracker();
